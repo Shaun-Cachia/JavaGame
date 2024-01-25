d797685178333ca61bbbeb5a63ce9788c2d3a4e9
Kayel Calleja <129683809+KayelC@users.noreply.github.com>
2024-01-25 14:19:47 +0100

    Create Player.java
    Bare Bones Structure Ta Player Class

---
+++ Player.java
@@ -0,0 +1,20 @@
+public class Player extends Character {
+
+  // Player Specific Constructor
+  public Player(int EXP, int LV, int HP, int AP, int Pwr, int Def, int Int, int Agl, String Master){
+    super(EXP, LV, HP, AP, Pwr, Def, Int, Agl, Master); // Calls Constructor From Super Class
+    // TODO Auto Generated B Constructor tal Math class
+  }
+
+
+  // Player Specific Methods
+  public int attack() {
+    // TODO Auto Generated B Method tal Math class
+    return 0;
+  }
+
+  public int defend() {
+    // TODO Auto Generated B Method tal Math class
+  return 0;
+  }
+}
